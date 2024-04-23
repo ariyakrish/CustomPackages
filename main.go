774a359db0842954b7646f035c710cd4168a8f97
ariya <ariyakrishnan.s@intelizign.com>
2024-04-23 17:20:42 +0530

    g

--- main.go
+++ main.go
@@ -15,11 +15,10 @@ func Add(a, b int) int {
 
 // Add takes two integers and returns their sum.
 func CreateFile()  {
-    f, err := os.Create("package.txt")
+    _, err := os.Create("package.txt")
     if err != nil {
         log.Fatal(err)
     }
-    defer f.Close()
     fmt.Println("File created:", "package.txt")
 
 }
