ac1c00a0491de36553330015e82a99e193ae2f4c
ariya <ariyakrishnan.s@intelizign.com>
2024-04-23 17:44:49 +0530

    aaa

--- main.go
+++ main.go
@@ -2,7 +2,9 @@
 
 package CustomPackages
 
-
+import (
+    "os"
+)
 
 // Add takes two integers and returns their sum.
 func Add(a, b int) int {
@@ -12,3 +14,10 @@ func Sub(a, b int) int {
     return a - b
 }
 
+func CreateFile() string{
+    _, err := os.create("Sample.txt")
+    if err!= nil{
+        return err
+    }
+    return "File Created Succesfully"
+}
