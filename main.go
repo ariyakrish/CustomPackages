1ccc5826ef88e19f92f04274196f97fdaa9ba227
ariya <ariyakrishnan.s@intelizign.com>
2024-04-23 17:07:27 +0530

    h

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import  ( "os"
 
 )
 
+// Add takes two integers and returns their sum.
+func Add(a, b int) int {
+    return a + b
+}
 
 // Add takes two integers and returns their sum.
 func CreateFile()  {
