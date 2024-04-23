abd97d9d15e9b08b1595dff84fd33e65cd80f840
ariya <ariyakrishnan.s@intelizign.com>
2024-04-23 17:27:01 +0530

    hsgd

--- main.go
+++ main.go
@@ -2,22 +2,11 @@
 
 package CustomPackages
 
-import  ( "os"
-"log"
 
-)
 
 // Add takes two integers and returns their sum.
 func Add(a, b int) int {
     return a + b
 }
 
-// Add takes two integers and returns their sum.
-func CreateFile() string {
-    _, err := os.Create("package.txt")
-    if err != nil {
-        return err
-    }
-    return "File created: package.txt"
 
-}
