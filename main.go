67660ad5cae93bf91694d84d5e6661bc4e830346
ariya <ariyakrishnan.s@intelizign.com>
2024-04-23 17:48:49 +0530

    aaa

--- main.go
+++ main.go
@@ -13,11 +13,25 @@ func Add(a, b int) int {
 func Sub(a, b int) int {
     return a - b
 }
-
-func CreateFile() string{
-    _, err := os.create("Sample.txt")
-    if err!= nil{
-        return err
+ 
+func CreateFile(FileName string) (*os.File, error) {
+    // Check if the file exists
+    _, err := os.Stat(FileName)
+    if err == nil {
+        return nil, os.ErrExist // File already exists
     }
-    return "File Created Succesfully"
+    if !os.IsNotExist(err) {
+        return nil, err // Unexpected error occurred while checking file existence
+    }
+ 
+    // File doesn't exist, proceed to create it
+    file, err := os.Create(FileName)
+    if err != nil {
+        return nil, err
+    }
+ 
+    // Ensure the file is closed when the function returns
+    defer file.Close()
+ 
+    return file, nil
 }
